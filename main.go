183d82021731331cf59c3c8561b38a4301e8debd
James Digrazia <jtd2401@rit.edu>
2019-05-23 10:54:43 -0400

    Hello world

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hiya")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Requested: %s\n", r.URL.Path)
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
